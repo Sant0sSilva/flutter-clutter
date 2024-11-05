b4085c30fa3db270d4a998321bb06d37eea58b4d
Sant0sSilva <lourencos88@gmail.com>
2024-11-05 15:50:45 -0500

    Initial commit of roll_dice project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+        Color.fromRGBO(10, 2, 2, 5),
+        Color.fromRGBO(90, 4, 4, 5),
+        ),
+      ),
+    ),
+  );
+}
